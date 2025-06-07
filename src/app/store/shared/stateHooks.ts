0190ddb76b53ce5213c47f92b71f74cbab80c309
RoRoStudio <robert@rorostudio.com>
2025-06-07 21:22:47 +0200

    removing files

--- src/app/store/shared/stateHooks.ts
+++
@@ -1,8 +0,0 @@
-// src/store/shared/stateHooks.ts - This file will redirect to new locations
-export * from '../../../features/actions/store/useActionActions';
-export * from '../../../features/notes/store/useNoteActions';
-export * from '../../../features/paths/store/usePathActions';
-export * from '../../../features/sagas/store/useSagaActions';
-export * from '../../../features/sparks/store/useSparkActions';
-// TODO: Add loops when implemented
-// export * from '../../../features/loops/store/useLoopActions'; 
